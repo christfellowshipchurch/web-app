95a9f87c5fb35a35651e6958b08ac9258dc6f1ee
Caleb Panza <caleb.panza@icloud.org>
2019-09-06 11:57:02 -0400

    Updated Block to use the new format to allow for inline videos to be manually played

--- src/components/ui/ContentBlock/ContentBlock.js
+++ src/components/ui/ContentBlock/ContentBlock.js
@@ -4,90 +4,94 @@ import { lowerCase, get } from 'lodash'
 import { Block, Media, Button } from '@christfellowshipchurch/web-ui-kit'
 
 const titleClasses = classnames(
-    'font-weight-bold'
-  )
-  const subtitleClasses = classnames(
-    'pt-1'
-  )
-  const bodyClasses = classnames(
-    'pt-1',
-    'pb-4',
-    'px-3'
-  )
+  'font-weight-bold'
+)
+const subtitleClasses = classnames(
+  'pt-1'
+)
+const bodyClasses = classnames(
+  'pt-1',
+  'pb-4',
+  'px-3'
+)
 
 const ButtonRow = ({ callToAction, secondaryCallToAction }) => (
-    <div>
-        {callToAction && callToAction.call !== '' && <Button type='primary' call={callToAction.call} />}
-        <br></br>
-        {secondaryCallToAction && secondaryCallToAction !== '' && <Button type='link' call={secondaryCallToAction.call} />}
-    </div>
+  <div>
+    {callToAction && callToAction.call !== '' && <Button type='primary' call={callToAction.call} />}
+    <br></br>
+    {secondaryCallToAction && secondaryCallToAction !== '' && <Button type='link' call={secondaryCallToAction.call} />}
+  </div>
 )
 
 const ContentBlock = ({
-    contentLayout,
-    images,
-    videos,
-    imageAlt,
-    videoUrl,
-    imageRatio,
-    subtitle,
-    title,
-    htmlContent,
-    callToAction,
-    secondaryCallToAction
-  }) => (lowerCase(contentLayout) === 'background'
-    ? (
-      <Media
-        imageUrl={get(images, '[0].sources[0].uri', '')}
-        imageAlt={imageAlt}
-        videoUrl={get(videos, '[0].sources[0].uri', '')}
-        ratio={imageRatio}
-      >
-        <Block contentLayout="default" className="text-light">
-          <Block.Title className='display-4'>
-            {title}
-          </Block.Title>
-  
-          <Block.Subtitle className=''>
+  contentLayout,
+  images,
+  videos,
+  imageAlt,
+  imageRatio,
+  subtitle,
+  title,
+  htmlContent,
+  callToAction,
+  secondaryCallToAction
+}) => (lowerCase(contentLayout) === 'background'
+  ? (
+    <Media
+      imageUrl={get(images, '[0].sources[0].uri', '')}
+      imageAlt={imageAlt}
+      videoUrl={get(videos, '[0].sources[0].uri', '')}
+      ratio={imageRatio}
+    >
+      <Block contentLayout="default" className="text-light">
+        <Block.Title className='display-4'>
+          {title}
+        </Block.Title>
+
+        <Block.Subtitle className=''>
+          {subtitle}
+        </Block.Subtitle>
+
+        <Block.Body className={bodyClasses}>
+          {htmlContent}
+        </Block.Body>
+
+        <ButtonRow callToAction={callToAction} secondaryCallToAction={secondaryCallToAction} />
+      </Block>
+    </Media>
+  )
+  : (
+    <div className="container py-5">
+      <div className="row">
+        <Block
+          layout={lowerCase(contentLayout)}
+          media={get(images, '[0].sources[0].uri', null) || get(videos, '[0].sources[0].uri', null)
+            ? {
+              imageUrl: get(images, '[0].sources[0].uri', ''),
+              imageAlt,
+              videoUrl: get(videos, '[0].sources[0].uri', ''),
+              ratio: imageRatio,
+              showControls: true,
+              rounded: true
+            } : null}
+        >
+
+          <Block.Subtitle className={subtitleClasses}>
             {subtitle}
           </Block.Subtitle>
-  
+
+          <Block.Title className={titleClasses}>
+            {title}
+          </Block.Title>
+
           <Block.Body className={bodyClasses}>
             {htmlContent}
           </Block.Body>
-  
+
           <ButtonRow callToAction={callToAction} secondaryCallToAction={secondaryCallToAction} />
         </Block>
-      </Media>
-    )
-    : (
-      <div className="container py-5">
-        <div className="row">
-          <Block
-            layout={lowerCase(contentLayout)}
-            imageUrl={get(images, '[0].sources[0].uri', '')}
-            imageAlt={imageAlt}
-            videoUrl={get(videos, '[0].sources[0].uri', '')}
-            ratio={imageRatio}
-          >
-  
-            <Block.Subtitle className={subtitleClasses}>
-              {subtitle}
-            </Block.Subtitle>
-  
-            <Block.Title className={titleClasses}>
-              {title}
-            </Block.Title>
-  
-            <Block.Body className={bodyClasses}>
-              {htmlContent}
-            </Block.Body>
-  
-            <ButtonRow callToAction={callToAction} secondaryCallToAction={secondaryCallToAction} />
-          </Block>
-        </div>
       </div>
-    )
-    )
+    </div>
+  )
+  )
 
-    export default ContentBlock
+export default ContentBlock
