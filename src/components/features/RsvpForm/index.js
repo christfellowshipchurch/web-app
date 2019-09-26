0b916a21051169eee6538a563be7da57a7ce8197
Caleb Panza <caleb.panza@icloud.org>
2019-09-26 18:04:44 -0400

    Phone Number validation

--- src/components/features/RsvpForm/index.js
+++ src/components/features/RsvpForm/index.js
@@ -6,7 +6,8 @@ import AwesomePhoneNumber from 'awesome-phonenumber'
 import {
     get,
     filter,
-    has
+    has,
+    keys
 } from 'lodash'
 import { Button } from '@christfellowshipchurch/web-ui-kit'
 import { ContactForm, DemographicForm, VisitForm } from './fragments'
@@ -27,9 +28,18 @@ const Rsvp = (props) => {
         submitting
     } = props
 
+    console.log({ errors }, errors.length)
+
     const [submitRsvp, { data, loading, error }] = useMutation(SUBMIT_RSVP)
     const filteredErrors = filter(errors, (n) => n !== DO_NOT_SHOW_ERROR)
     const emptyStrings = filter(values, n => n === '')
+
+    const disabled = keys(errors).length > 0
+        || emptyStrings.length > 0
+        || submitting
+        || loading
+
+    console.log(disabled)
 
     // Form submission successful
     if (get(data, 'submitRsvp', false)) {
@@ -112,7 +122,7 @@ const Rsvp = (props) => {
                 <div className="col text-center">
                     <Button
                         title={`Submit`}
-                        disabled={errors.length > 0 || emptyStrings.length > 0 || submitting || loading}
+                        disabled={disabled}
                         loading={submitting || loading}
                         onClick={() => submitRsvp({ variables: values })}
                     />
@@ -170,7 +180,7 @@ const RsvpForm = ({ initialValues }) => <Formik
                 'Please make sure you entered a valid phone number',
                 (value) => {
                     if (value) {
-                        const phoneNumber = new AwesomePhoneNumber(value)
+                        const phoneNumber = new AwesomePhoneNumber(`+1 ${value}`)
 
                         return phoneNumber.isValid()
                     }
