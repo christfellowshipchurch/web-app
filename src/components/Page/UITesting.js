4ed3968c2c4abd9c4338f3273f320f62dcbc4eff
Daniel Wood <dzwood327@gmail.com>
2019-10-16 14:14:50 -0400

    Added gradient and color option on Button

--- src/components/Page/UITesting.js
+++ src/components/Page/UITesting.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Feature } from '../features'
 import FloatingCard from '../../ui/FloatingCard'
-import Tabs from '../../ui/Tabs'
+import { Tabs, ButtonRow, Button} from '../../ui'
 
 const TEST_ARRAY = [0, 1, 2, 3,]
 // const TEST_ARRAY = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
@@ -9,24 +9,12 @@ const TEST_ARRAY = [0, 1, 2, 3,]
 const DefaultPage = () => {
 
   return (
-    <div className="container-fluid my-6">
-      <div className="row">
-        <div className="col">
-          <Tabs>
-            <div title="Test">
-              This is a test
-              </div>
-          </Tabs>
-
-          <div className="my-5"></div>
-
-          <Tabs>
-            {TEST_ARRAY.map((n, i) => (
-              <div title={`Test ${i}`} key={i}>
-                This is a test {i}
-              </div>
-            ))}
-          </Tabs>
+    <div className='container p-5 text-center'>
+      <div className='row'>
+        <div className='col'>
+          <ButtonRow callToAction/>
+          <br/>
+          <Button gradient color='warning'/>
         </div>
       </div>
     </div>
