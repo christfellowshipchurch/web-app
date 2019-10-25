79ff64674f49091bfd5ab44adc7479d0b4206f3b
Caleb Panza <caleb.panza@icloud.org>
2019-10-25 13:47:44 -0400

    Renamed style sheets and gitignore

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {
     BrowserRouter
 } from 'react-router-dom'
 
-import './styles/css/christ-fellowship-bootstrap.css'
+import './styles/css/styles.css'
 import {
     ClientProvider
 } from './client'
