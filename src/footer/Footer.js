6cfb1e3698f78bed6b43a6868d386b4ef1e11457
Caleb Panza <caleb.panza@icloud.org>
2020-03-21 19:31:50 -0400

    Update Footer.js

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -252,7 +252,7 @@ Footer.defaultProps = {
     ],
     resourceLinks: [
         { call: 'Church Online', action: 'https://live.christfellowship.church/' },
-        { call: 'Past Messages', action: 'https://www.youtube.com/user/christfellowship/videos' },
+        { call: 'Past Messages', action: 'https://www.youtube.com/playlist?list=PLUQ7jSnRB_efXMDq9Lka6stS02awWoaz4' },
         { call: 'Ministry Updates', action: '/items/ministry-updates-d5bf6fa745f26bd019ea35882ecab86c' },
         { call: 'Give Online', action: 'https://pushpay.com/g/christfellowship' },
     ],
