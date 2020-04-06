11f15998ea8f9ca405d9b7a30eea94ebf3a4da21
Caleb Panza <caleb.panza@icloud.org>
2020-04-06 12:44:22 -0400

    Added metadata handling to Content Single

--- src/content-single/index.js
+++ src/content-single/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useQuery } from 'react-apollo';
+import { get, uniqBy } from 'lodash';
 
-import { get } from 'lodash';
+import Metadata from '../metadata';
 import { GET_CONTENT_ITEM } from './queries';
 import UniversalContentItem from './UniversalContentItem';
 import EventContentItem from './EventContentItem';
@@ -17,43 +18,61 @@ const ContentSingle = ({ itemId }) => {
     });
 
     const content = get(data, 'node', {});
+    const metadata = get(data, 'metadata', []);
     let { __typename } = content;
     if (!__typename && itemId) {
         [__typename] = itemId.split(':');
     }
 
-    switch (__typename) {
-        case 'EventContentItem':
-            return (
-                <EventContentItem
-                    itemId={itemId}
-                    content={content}
-                    loading={loading}
-                    error={error}
-                />
-            );
-        case 'InformationalContentItem':
-            return (
-                <InformationalContentItem
-                    itemId={itemId}
-                    content={content}
-                    loading={loading}
-                    error={error}
-                />
-            );
-        case 'DevotionalContentItem':
-        case 'WeekendContentItem':
-        case 'UniversalContentItem':
-        default:
-            return (
-                <UniversalContentItem
-                    itemId={itemId}
-                    content={content}
-                    loading={loading}
-                    error={error}
-                />
-            );
-    }
+    const renderContentSingle = (typename) => {
+        switch (typename) {
+            case 'EventContentItem':
+                return (
+                    <EventContentItem
+                        itemId={itemId}
+                        content={content}
+                        loading={loading}
+                        error={error}
+                    />
+                );
+            case 'InformationalContentItem':
+                return (
+                    <InformationalContentItem
+                        itemId={itemId}
+                        content={content}
+                        loading={loading}
+                        error={error}
+                    />
+                );
+            case 'DevotionalContentItem':
+            case 'WeekendContentItem':
+            case 'UniversalContentItem':
+            default:
+                return (
+                    <UniversalContentItem
+                        itemId={itemId}
+                        content={content}
+                        loading={loading}
+                        error={error}
+                    />
+                );
+        }
+    };
+
+    return (
+        <>
+            <Metadata
+                tags={uniqBy([
+                    ...metadata,
+                    { name: 'title', content: get(content, 'title') },
+                    { name: 'description', content: get(content, 'summary') },
+                    { name: 'image', content: get(content, 'coverImage.sources[0].uri') },
+                    { name: 'og:video', content: get(content, 'videos[0].sources[0].uri') },
+                ], 'name')}
+            />
+            {renderContentSingle(__typename)}
+        </>
+    );
 };
 
 ContentSingle.propType = {
