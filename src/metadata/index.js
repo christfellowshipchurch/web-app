fd56aae1093a4f67391b19afc457804aa729acbe
Caleb Panza <caleb.panza@icloud.org>
2020-04-06 13:37:58 -0400

    Default images

--- src/metadata/index.js
+++ src/metadata/index.js
@@ -78,29 +78,11 @@ Metadata.propTypes = {
 Metadata.defaultProps = {
     tags: [
         { name: 'title', content: 'Christ Fellowship Church' },
+        { name: 'keywords', content: ['Church', 'Christ Fellowship', 'Christ Fellowship Church', 'Churches in South Florida'] },
         { name: 'description', content: "Christ Fellowship is a church in South Florida that helps you thrive in every area of life. Christ Fellowship Church's passion is to help you know God, grow in your relationship with God & others, and discover your purpose so that you can impact the world." },
         { name: 'url', content: 'https://christfellowship.church' },
         { name: 'og:type', content: 'website' },
-    ],
-};
-
-const defaultProps = {
-    title: defaultItems.title,
-    description: defaultItems.description,
-    keywords: ['Church', 'Christ Fellowship', 'Christ Fellowship Church', 'Churches in South Florida'],
-    openGraphProtocols: [
-        { name: 'title', content: defaultItems.title },
-        { name: 'description', content: defaultItems.description },
-        { name: 'image', content: '' },
-        { name: 'site_name', content: defaultItems.title },
-        { name: 'url', content: 'https://christfellowship.church' },
-        { name: 'type', content: 'website' },
-    ],
-    twitterProtocols: [
-        { name: 'title', content: defaultItems.title },
-        { name: 'description', content: defaultItems.description },
-        { name: 'image', content: '' },
-        { name: 'url', content: defaultItems.url },
+        { name: 'image', content: 'https://rock.christfellowship.church/Content/images/CF-Worship-Generic.jpg' },
     ],
 };
 
