60bd9451f4c8f9804ede9fe4f5bbdfc05c66ff0a
dzwood <46049974+dzwood@users.noreply.github.com>
2020-05-26 10:58:14 -0400

    Revert "Enable Login"
    This reverts commit 670720839c5f7d9284d13c65801ecf868d1fdea8.

--- src/router/Home/index.js
+++ src/router/Home/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import PageBuilder from '../../page-builder';
 import FeaturesFeed from '../../features-feed';
-import {
-     Redirect
-} from 'react-router-dom'
-
 
 import { useAuth } from '../../auth';
 
 const HomeRouter = () => {
     const { isLoggedIn } = useAuth();
 
-    return isLoggedIn &&
-        <Redirect to='/'/>
+    return isLoggedIn
+        ? <FeaturesFeed />
+        : (
+            <PageBuilder
+                title="home-page"
+                theme="swoop"
+            />
+        );
 };
 
 export default FeaturesFeed;
