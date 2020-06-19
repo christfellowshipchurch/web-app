5d95d3157866c8dc9e6604290a11edeb7518251a
Caleb Panza <caleb.panza@icloud.org>
2020-06-19 13:41:47 -0400

    Focus styling issue resolved

--- src/browse/FilterRow/index.js
+++ src/browse/FilterRow/index.js
@@ -1,14 +1,12 @@
-import React from 'react'
-import classnames from 'classnames'
-import PropTypes from 'prop-types'
+import React from 'react';
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
 
 const BrowseFilters = ({
     selected,
     onChange,
-    filters
-}) => {
-
-    return (
+    filters,
+}) => (
         <ul
             className="list-inline text-nowrap overflow-x-scroll mb-n4"
         >
@@ -20,7 +18,8 @@ const BrowseFilters = ({
                     <a
                         href="#"
                         style={{
-                            border: selected != n.id ? '1px solid #d6d6d6' : 'none'
+                            border: selected != n.id ? '1px solid #d6d6d6' : 'none',
+                            ...(selected === n.id ? { color: 'white' } : {}),
                         }}
                         className={classnames(
                             'h5',
@@ -30,14 +29,14 @@ const BrowseFilters = ({
                             'focus-indicator-none',
                             'font-weight-normal',
                             {
-                                'text-white badge-primary': selected === n.id,
-                                'text-dark badge-light': selected !== n.id,
-                            }
+                                'badge-primary': selected === n.id,
+                                'badge-light': selected !== n.id,
+                            },
                         )}
 
                         onClick={(e) => {
-                            e.preventDefault()
-                            onChange({ id: n.id })
+                            e.preventDefault();
+                            onChange({ id: n.id });
                         }}
                     >
                         {n.title}
@@ -45,18 +44,17 @@ const BrowseFilters = ({
                 </li>
             ))}
         </ul>
-    )
-}
+    );
 
 BrowseFilters.propTypes = {
     selected: PropTypes.string,
     onChange: PropTypes.func,
-    filters: PropTypes.array
-}
+    filters: PropTypes.array,
+};
 
 BrowseFilters.defaultProps = {
     filter: null,
     title: null,
-}
+};
 
-export default BrowseFilters
+export default BrowseFilters;
