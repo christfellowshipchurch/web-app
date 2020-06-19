b5f5ce4d8f897b0cba462ff7eec71b4d02755277
Caleb Panza <caleb.panza@icloud.org>
2020-06-19 14:02:42 -0400

    Button style update for Profile icon

--- src/navbar/Navbar.sandbox.js
+++ src/navbar/Navbar.sandbox.js
@@ -1,54 +1,55 @@
-import React, { useState } from 'react'
-import { useQuery } from 'react-apollo'
-import PropTypes from 'prop-types'
-import classnames from 'classnames'
-import { isMobile, isIOS } from 'react-device-detect'
-import { get, includes, camelCase } from 'lodash'
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
+import React, { useState } from 'react';
+import { useQuery } from 'react-apollo';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import { isMobile, isIOS } from 'react-device-detect';
+import { get, includes, camelCase } from 'lodash';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 
-import { Icon } from '../ui/Icons'
-import { useAuth } from '../auth'
+import { Icon } from '../ui/Icons';
+import { useAuth } from '../auth';
 
-import { GET_WEBSITE_HEADER_LOGGED_IN, GET_PROFILE_IMAGE } from './queries'
-import ProfileConnected from './AuthNavbar/ProfileConnected'
-import DynamicBanner from './DynamicBanner'
-import DesktopNav from './DesktopNav.sandbox'
-import MobileNav from './MobileNav.sandbox'
+import { GET_WEBSITE_HEADER_LOGGED_IN, GET_PROFILE_IMAGE } from './queries';
+import ProfileConnected from './AuthNavbar/ProfileConnected';
+import DynamicBanner from './DynamicBanner';
+import DesktopNav from './DesktopNav.sandbox';
+import MobileNav from './MobileNav.sandbox';
 
 // Takes a collection of images from the API's return data and formats
 //  it to be an array of the following object structure: { imageKey: { uri, alt } }
 const imageArrayToObject = (images) => {
-  let imagesObj = {}
+  const imagesObj = {};
 
   images.forEach((n, i) => {
-    const key = camelCase(get(n, 'name', i))
-    const uri = get(n, 'sources[0].uri', '')
-    const alt = get(n, 'name', 'Christ Fellowship Church')
+    const key = camelCase(get(n, 'name', i));
+    const uri = get(n, 'sources[0].uri', '');
+    const alt = get(n, 'name', 'Christ Fellowship Church');
 
-    imagesObj[key] = { uri, alt }
-  })
+    imagesObj[key] = { uri, alt };
+  });
 
-  return imagesObj
-}
+  return imagesObj;
+};
 
 const BrandImg = ({
   className,
   uri,
-  alt
-}) =>
-  <Navbar.Brand
-    href="/"
-    className={classnames(
-      "align-self-start",
-      className
-    )}
-  >
-    <img
-      src={uri}
-      style={{ height: '58px', width: 'auto' }}
-      alt={alt}
-    />
-  </Navbar.Brand>
+  alt,
+}) => (
+    <Navbar.Brand
+      href="/"
+      className={classnames(
+        'align-self-start',
+        className,
+      )}
+    >
+      <img
+        src={uri}
+        style={{ height: '58px', width: 'auto' }}
+        alt={alt}
+      />
+    </Navbar.Brand>
+  );
 
 const NavbarConnected = ({
   bg,
@@ -57,37 +58,38 @@ const NavbarConnected = ({
   onToggle,
   fixed,
   navLinks,
-  quickAction
+  quickAction,
 }) => {
-  const { logout, logIn, isLoggedIn } = useAuth()
-  const [menuIcon, setMenuIcon] = useState(false)
-  const website = process.env.REACT_APP_WEBSITE_KEY
+  const { logout, logIn, isLoggedIn } = useAuth();
+  const [menuIcon, setMenuIcon] = useState(false);
+  const website = process.env.REACT_APP_WEBSITE_KEY;
   const { loading, error, data } = useQuery(GET_WEBSITE_HEADER_LOGGED_IN, {
     variables: { website },
-    fetchPolicy: "cache-and-network"
-  })
+    fetchPolicy: 'cache-and-network',
+  });
 
-  navLinks = get(data, 'getWebsiteNavigation.navigationLinks', [])
-  quickAction = get(data, 'getWebsiteNavigation.quickAction', null)
+  navLinks = get(data, 'getWebsiteNavigation.navigationLinks', []);
+  quickAction = get(data, 'getWebsiteNavigation.quickAction', null);
 
-  const images = imageArrayToObject(get(data, 'getWebsiteNavigation.images', []))
-  const brandImage = get(images, brandImageKey, null)
+  const images = imageArrayToObject(get(data, 'getWebsiteNavigation.images', []));
+  const brandImage = get(images, brandImageKey, null);
   const navbarProps = {
     bg,
     variant,
-    expand: 'lg'
-  }
+    expand: 'lg',
+  };
 
   // We use sticky styling as the default so that padding is respected
   //    with the option to override it to use a fixed styling if preferred
-  if (fixed) navbarProps.fixed = "top"
-  else navbarProps.sticky = "top"
+  if (fixed) navbarProps.fixed = 'top';
+  else navbarProps.sticky = 'top';
 
-  const dropDownIcon = (isOpen) => 
-    <Icon 
-      name={isOpen ? 'times' : 'bars'} 
-      fill="#525252" 
+  const dropDownIcon = (isOpen) => (
+    <Icon
+      name={isOpen ? 'times' : 'bars'}
+      fill="#525252"
     />
+  );
 
   return (
     <Navbar
@@ -96,11 +98,11 @@ const NavbarConnected = ({
       className={classnames(
         'd-flex',
         'flex-column',
-        'p-0'
+        'p-0',
       )}
     >
 
-      <div className='row w-100'>
+      <div className="row w-100">
         <DynamicBanner />
       </div>
 
@@ -109,17 +111,18 @@ const NavbarConnected = ({
           'row',
           'w-100',
           'd-flex',
-          'justify-content-between'
+          'justify-content-between',
         )}
       >
         {/* Mobile Brand Image */}
-        {brandImage &&
-          <BrandImg
-            uri={brandImage.uri}
-            alt={brandImage.alt}
-            className="d-lg-none p-2"
-          />
-        }
+        {brandImage
+          && (
+            <BrandImg
+              uri={brandImage.uri}
+              alt={brandImage.alt}
+              className="d-lg-none p-2"
+            />
+          )}
 
         <Navbar.Toggle
           aria-controls="basic-navbar-nav"
@@ -128,7 +131,7 @@ const NavbarConnected = ({
         >
           <span
             onClick={() => setMenuIcon(!menuIcon)}
-            className='mr-2'
+            className="mr-2"
           >
             {dropDownIcon(menuIcon)}
           </span>
@@ -143,87 +146,91 @@ const NavbarConnected = ({
 
               'flex-lg-row',
               'justify-content-lg-between',
-              "align-items-lg-center",
+              'align-items-lg-center',
 
               'w-100',
-              "px-lg-3",
+              'px-lg-3',
             )}
           >
 
             {/* Desktop Brand Image */}
             <div>
-              {brandImage &&
-                <BrandImg
-                  uri={brandImage.uri}
-                  alt={brandImage.alt}
-                  className={classnames(
-                    'd-none',
-                    'd-lg-block',
-                    'pl-4'
-                  )}
-                />
-              }
+              {brandImage
+                && (
+                  <BrandImg
+                    uri={brandImage.uri}
+                    alt={brandImage.alt}
+                    className={classnames(
+                      'd-none',
+                      'd-lg-block',
+                      'pl-4',
+                    )}
+                  />
+                )}
             </div>
 
-            <div className='d-flex align-items-center'>
+            <div className="d-flex align-items-center">
 
-              {quickAction &&
-                <Nav.Link
-                  href={quickAction.action}
-                  className={classnames(
-                    'mx-lg-2',
-                    'mx-4',
-                    'my-2',
-                    'px-3',
-                    'btn-primary',
-                    'nav-link',
-                    'text-white',
-                    'rounded'
-                  )}
-                >
-                  {quickAction.call}
-                </Nav.Link>
-              }
+              {quickAction
+                && (
+                  <Nav.Link
+                    href={quickAction.action}
+                    className={classnames(
+                      'mx-lg-2',
+                      'mx-4',
+                      'my-2',
+                      'px-3',
+                      'btn-primary',
+                      'nav-link',
+                      'text-white',
+                      'rounded',
+                    )}
+                  >
+                    {quickAction.call}
+                  </Nav.Link>
+                )}
               {isLoggedIn
-                ? <ProfileConnected 
-                    className='d-none d-lg-block ml-2 mr-1'
+                ? (
+                  <ProfileConnected
+                    className="d-none d-lg-block ml-2 mr-1"
                   />
-                : 
+                )
+                : (
                   <Icon
                     onClick={() => logIn()}
-                    className='d-none d-lg-block ml-2 mr-1'
-                    name='user-circle'
-                    fill='#525252'
+                    className="d-none d-lg-block ml-2 mr-1 btn-like"
+                    name="user-circle"
+                    fill="#525252"
                     size={32}
                   />
-              }
+                )}
 
               <DesktopNav
                 links={navLinks}
                 auth={{
-                  isLoggedIn: isLoggedIn,
+                  isLoggedIn,
                   logIn: () => logIn(),
-                  logout: () => logout()
+                  logout: () => logout(),
                 }}
-              /> 
+              />
             </div>
 
             <MobileNav
               links={navLinks}
               auth={{
-                isLoggedIn: isLoggedIn,
+                isLoggedIn,
                 logIn: () => logIn(),
-                logout: () => logout()
+                logout: () => logout(),
               }}
             />
-                
-         
-          </div> 
+
+
+          </div>
         </Navbar.Collapse>
       </div>
     </Navbar>
-  )
-}
+  );
+};
 
 NavbarConnected.propTypes = {
   bg: PropTypes.string,
@@ -234,8 +241,9 @@ NavbarConnected.propTypes = {
     PropTypes.shape({
       call: PropTypes.string,
       action: PropTypes.string,
-    }))
-}
+    }),
+  ),
+};
 
 NavbarConnected.defaultProps = {
   bg: 'white',
@@ -247,6 +255,6 @@ NavbarConnected.defaultProps = {
     { call: 'Church Locations', action: '/locations' },
     { call: 'Request Prayer', action: 'https://gochristfellowship.com/new-here/contact-us/' },
   ],
-}
+};
 
-export default NavbarConnected
+export default NavbarConnected;
