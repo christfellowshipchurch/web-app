c60ce1cf2cb256c32d97ff74e0d220fcbf851e58
dzwood <46049974+dzwood@users.noreply.github.com>
2020-06-23 15:15:46 -0400

    formatted campus name properly to query campus correctly

--- src/router/Campuses/index.js
+++ src/router/Campuses/index.js
@@ -2,13 +2,17 @@ import React from 'react'
 import {
     Switch, Route, Redirect
 } from 'react-router-dom'
+import { capitalize, join } from 'lodash'
 
 import { CampusPageBuilder } from '../../campus'
 import PageBuilder from '../../page-builder'
 
-const PropertyMapper = ({ match: { params: { name } } }) =>
-    <CampusPageBuilder name={name} />
+const PropertyMapper = ({ match: { params: { name } } }) =>{
 
+    const formattedName = join(name.split('-').map( n => capitalize(n)), ' ')
+
+    return <CampusPageBuilder name={formattedName} />
+}
 const LocationsPage = () => <PageBuilder
     title="locations"
     theme="swoop"
