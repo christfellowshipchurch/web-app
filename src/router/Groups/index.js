2c22053cfd503e90bbdcf6e5b185f7a3ba70bd69
Burke Shartsis <burke@differential.com>
2020-08-20 21:56:20 -0400

    enable "group single" route

--- src/router/Groups/index.js
+++ src/router/Groups/index.js
@@ -1,32 +1,30 @@
 import React from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
-// import GroupConnected from './GroupConnected';
-import { Groups } from '../../groups';
+import { Groups, GroupContentItemConnected } from '../../groups';
 
-// import redirects from '../../redirects.json';
+import redirects from '../../redirects.json';
 
-// const GroupDetailLayout = ({
-//   match: {
-//     params: { groupName },
-//   },
-// }) => {
-//   const page = groupName;
-//   const redirect = redirects[decodeURI(page)];
-//
-//   // check if the redirects.json file has a redirect for this page
-//   if (!!redirect && redirect !== '') {
-//     window.location.href = redirect;
-//     return null; // return null so nothing is rendered while the redirect is happening
-//   }
-//
-//   // if no redirect, proceed to the Router
-//   return <GroupConnected title={groupName} />;
-// };
+const GroupSingle = ({
+  match: { params: { groupName } = {} } = {},
+  location: { state: { contentId } = {} } = {},
+}) => {
+  const page = groupName;
+  const redirect = redirects[decodeURI(page)];
+
+  // check if the redirects.json file has a redirect for this page
+  if (!!redirect && redirect !== '') {
+    window.location.href = redirect;
+    return null; // return null so nothing is rendered while the redirect is happening
+  }
+
+  // if no redirect, proceed to the Router
+  return <GroupContentItemConnected itemId={contentId} title={groupName} />;
+};
 
 const Router = () => (
   <Switch>
-    {/* <Route exact path="/groups/:groupName" component={GroupDetailLayout} /> */}
+    <Route exact path="/groups/:groupName" component={GroupSingle} />
 
     <Route path="*" component={Groups} />
   </Switch>
