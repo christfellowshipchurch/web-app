7edf17f8c9fe2cfda435b058e12813f2e4733372
Burke Shartsis <burke@differential.com>
2020-08-20 21:54:45 -0400

    export GroupContentItemConnected /groups

--- src/groups/index.js
+++ src/groups/index.js
@@ -1,2 +1,3 @@
-export { default as GroupListConnected } from './GroupListConnected';
 export { default as Groups } from './Groups';
+export { default as GroupContentItemConnected } from './GroupContentItemConnected';
+export { default as GroupListConnected } from './GroupListConnected';
