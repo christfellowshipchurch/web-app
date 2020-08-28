50a0164712642cc477825c5a11c7ef3e9e808eab
Ryan Davidson <ryan@rkdavidson.com>
2020-08-28 15:33:57 -0400

    Switch channelId to be the test 24 hr stream event instead

--- src/content-single/EventContentItem/components/EventChat.js
+++ src/content-single/EventContentItem/components/EventChat.js
@@ -26,30 +26,33 @@ const users = {
       image:
         "https://cloudfront.christfellowship.church/GetImage.ashx?guid=2cb1a60f-f346-49a5-b192-be1b84875bea",
     },
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiM2E0YTIwZjA4MjhjNTkyZjdmMzY2ZGZjZThkMWY5YWIifQ.FbFhBRt07KWZOp2ZqYQMveGtpvIqXjM5oCUNmHQHdas"
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiM2E0YTIwZjA4MjhjNTkyZjdmMzY2ZGZjZThkMWY5YWIifQ.FbFhBRt07KWZOp2ZqYQMveGtpvIqXjM5oCUNmHQHdas",
   ],
   yoda: [
     {
       id: "3fd1595b8f555c2e1c2f1a57d2947898",
       name: "Yoda",
-      image:
-        "https://avatarfiles.alphacoders.com/218/thumb-218445.jpg",
+      image: "https://avatarfiles.alphacoders.com/218/thumb-218445.jpg",
     },
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiM2ZkMTU5NWI4ZjU1NWMyZTFjMmYxYTU3ZDI5NDc4OTgifQ.SkFGfHGd8hg4jz0xyc7RWwi2WyJlgKjaADxft5mlh0Q'
-  ]
-}
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiM2ZkMTU5NWI4ZjU1NWMyZTFjMmYxYTU3ZDI5NDc4OTgifQ.SkFGfHGd8hg4jz0xyc7RWwi2WyJlgKjaADxft5mlh0Q",
+  ],
+};
 
 chatClient.setUser(...users.yoda);
 
-const channel = chatClient.channel("livestream", "ryan-test-channel", {
-  image: "https://cloudfront.christfellowship.church/GetImage.ashx?guid=3588831c-d389-4b7a-98ca-9476b8be2066",
+// const channelId = "ryan-test-channel";
+const channelId = "1e07e4f2e1b52807e43b2338c73d88ba";
+
+const channel = chatClient.channel("livestream", channelId, {
+  image:
+    "https://cloudfront.christfellowship.church/GetImage.ashx?guid=3588831c-d389-4b7a-98ca-9476b8be2066",
   name: "Ryan's Livestream Chat Test",
 });
 
-console.log('[rkd] channel:', channel);
+console.log("[rkd] channel:", channel);
 
 const EventChat = ({ }) => (
-  <Chat client={chatClient} theme={"livestream"}>
+  <Chat client={chatClient} theme="livestream">
     <Channel channel={channel} Message={MessageLivestream}>
       <Window>
         <ChannelHeader live />
