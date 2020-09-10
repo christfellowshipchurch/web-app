bde32434c5b1d701732c600031f5002f433d41e3
Burke Shartsis <burke@differential.com>
2020-09-10 18:00:00 -0400

    fix add to calendar dates always being all day
    - also adds helpful info to the description field.

--- src/groups/GroupContentItemConnected/GroupContentItem.js
+++ src/groups/GroupContentItemConnected/GroupContentItem.js
@@ -40,109 +40,123 @@ const GroupContentItem = ({
   title,
   userName,
   videoCall,
-}) => (
-  <>
-    <Banner coverImage={coverImage} shareTitle="Invite" title={title} />
-    <div className="container-fluid mb-4 px-3">
-      <hgroup className="mt-4 mb-3 pb-3">
-        <h1 className="mb-2 text-dark">{title}</h1>
-        <h3 className="mt-1 content-subtitle font-weight-light">{dateText}</h3>
-      </hgroup>
+}) => {
+  let calendarLinkDescription = `Join us for ${title} at Christ Fellowship!\n\n`;
 
-      <section className="row mx-n2">
-        <aside className={classnames('col-12', { 'col-lg-4': summary }, 'p-2')}>
-          <Card key="EventOccurences" className="mb-3">
-            {get(parentVideoCall, 'link') && (
-              <a
-                className="btn btn-primary btn-block mb-3"
-                href={videoCallURLWithParameters(
-                  get(parentVideoCall, 'link'),
-                  userName
-                    ? {
-                        uname: userName,
-                      }
-                    : null
-                )}
-                onClick={() => onClickParentVideoCall('parent')}
-                target="_blank"
-              >
-                {get(parentVideoCall, 'labelText') || `Join Meeting`}
-              </a>
-            )}
-            {get(videoCall, 'link') ? (
-              <a
-                className="btn btn-primary btn-block mb-3"
-                href={videoCallURLWithParameters(
-                  get(videoCall, 'link'),
-                  userName
-                    ? {
-                        uname: userName,
-                      }
-                    : null
-                )}
-                onClick={() => onClickVideoCall()}
-                target="_blank"
-              >
-                {get(videoCall, 'label') || parentVideoCall
-                  ? 'Join Breakout'
-                  : 'Join Meeting'}
-              </a>
-            ) : (
-              <a
-                // allow to just checkin for in person groups
-                className="btn btn-primary btn-block mb-3"
-                href={''}
-                onClick={() => onClickVideoCall()}
-              >
-                Check In
-              </a>
-            )}
-            {groupResources.map((resource, i) => (
-              <a
-                key={resource.url}
-                className={classnames('btn', 'btn-outline-dark', 'btn-block', 'my-3')}
-                href={resource.url}
-                onClick={() =>
-                  onClickGroupResource({
-                    resourceTitle: resource.title,
-                  })
-                }
-                target={resource.url.includes('http') ? '_blank' : ''}
-              >
-                {resource.title}
-              </a>
-            ))}
-            <div className="d-flex align-items-center">
-              <Icon name="calendar-plus" className="mr-2" />
-              <AddToCalendar
-                className={classnames('p-0', 'text-dark', 'font-weight-bold')}
-                style={{
-                  fontSize: '1.125rem',
-                  letterSpacing: 'normal',
-                }}
-                event={{
-                  title,
-                  summary,
-                  // Location is the webUrl for now because we have multiple potential video calls endpoints
-                  address: document.URL,
-                  startTime: dateTimes.start,
-                  endTime: dateTimes.end,
-                }}
-                alternateDescription={`Join us for ${title} at Christ Fellowship!`}
-                allDay
-              />
+  if (parentVideoCall) {
+    calendarLinkDescription += get(parentVideoCall, 'labelText') || `Join Meeting`;
+    calendarLinkDescription += `\n${get(parentVideoCall, 'link', '')}\n\n`;
+  } else if (videoCall) {
+    calendarLinkDescription +=
+      get(videoCall, 'label') || parentVideoCall ? 'Join Breakout' : 'Join Meeting';
+
+    calendarLinkDescription += `\n${get(videoCall, 'link', '')}`;
+  }
+
+  return (
+    <>
+      <Banner coverImage={coverImage} shareTitle="Invite" title={title} />
+      <div className="container-fluid mb-4 px-3">
+        <hgroup className="mt-4 mb-3 pb-3">
+          <h1 className="mb-2 text-dark">{title}</h1>
+          <h3 className="mt-1 content-subtitle font-weight-light">{dateText}</h3>
+        </hgroup>
+
+        <section className="row mx-n2">
+          <aside className={classnames('col-12', { 'col-lg-4': summary }, 'p-2')}>
+            <Card key="EventOccurences" className="mb-3">
+              {get(parentVideoCall, 'link') && (
+                <a
+                  className="btn btn-primary btn-block mb-3"
+                  href={videoCallURLWithParameters(
+                    get(parentVideoCall, 'link'),
+                    userName
+                      ? {
+                          uname: userName,
+                        }
+                      : null
+                  )}
+                  onClick={() => onClickParentVideoCall('parent')}
+                  target="_blank"
+                >
+                  {get(parentVideoCall, 'labelText') || `Join Meeting`}
+                </a>
+              )}
+              {get(videoCall, 'link') ? (
+                <a
+                  className="btn btn-primary btn-block mb-3"
+                  href={videoCallURLWithParameters(
+                    get(videoCall, 'link'),
+                    userName
+                      ? {
+                          uname: userName,
+                        }
+                      : null
+                  )}
+                  onClick={() => onClickVideoCall()}
+                  target="_blank"
+                >
+                  {get(videoCall, 'label') || parentVideoCall
+                    ? 'Join Breakout'
+                    : 'Join Meeting'}
+                </a>
+              ) : (
+                <a
+                  // allow to just checkin for in person groups
+                  className="btn btn-primary btn-block mb-3"
+                  href={''}
+                  onClick={() => onClickVideoCall()}
+                >
+                  Check In
+                </a>
+              )}
+              {groupResources.map((resource, i) => (
+                <a
+                  key={resource.url}
+                  className={classnames('btn', 'btn-outline-dark', 'btn-block', 'my-3')}
+                  href={resource.url}
+                  onClick={() =>
+                    onClickGroupResource({
+                      resourceTitle: resource.title,
+                    })
+                  }
+                  target={resource.url.includes('http') ? '_blank' : ''}
+                >
+                  {resource.title}
+                </a>
+              ))}
+              <div className="d-flex align-items-center">
+                <Icon name="calendar-plus" className="mr-2" />
+                <AddToCalendar
+                  className={classnames('p-0', 'text-dark', 'font-weight-bold')}
+                  style={{
+                    fontSize: '1.125rem',
+                    letterSpacing: 'normal',
+                  }}
+                  event={{
+                    title,
+                    summary,
+                    // Location is the webUrl for now because we have multiple potential video calls endpoints
+                    address: document.URL,
+                    startTime: dateTimes.start,
+                    endTime: dateTimes.end,
+                    description: calendarLinkDescription,
+                  }}
+                  alternateDescription={calendarLinkDescription}
+                />
+              </div>
+            </Card>
+          </aside>
+          {summary ? (
+            <div className="col-12 col-lg-8 p-2">
+              <Card>{summary}</Card>
             </div>
-          </Card>
-        </aside>
-        {summary ? (
-          <div className="col-12 col-lg-8 p-2">
-            <Card>{summary}</Card>
-          </div>
-        ) : null}
-      </section>
-    </div>
-  </>
-);
+          ) : null}
+        </section>
+      </div>
+    </>
+  );
+};
 
 GroupContentItem.propTypes = {
   coverImage: PropTypes.shape({
