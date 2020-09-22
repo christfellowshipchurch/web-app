dad3a58f8b39eea523ce57a014933c28417b150f
Ryan Davidson <ryan@differential.com>
2020-09-21 22:32:06 -0400

    Created scaffold version of EventSchedule for EventPanel and update prop drilling

--- src/content-single/EventContentItem/EventDetail.sandbox.js
+++ src/content-single/EventContentItem/EventDetail.sandbox.js
@@ -12,16 +12,16 @@ const EventDetail = ({ title, summary, isLive }) => (
         <div className="row mt-3 pl-3">
           {isLive && <LiveIndicator />}
 
-          <div className="row">
+          <div className="col-12">
             <h1 className="mt-2 mb-2 text-dark">{title}</h1>
             <h3 className="mt-1 content-subtitle font-weight-light">{summary}</h3>
           </div>
-
-          <div className="row flex-grow-1 mt-4 justify-content-end">
-            <Share shareTitle="Invite" title={title} variant={'outline-dark'} />
-          </div>
         </div>
       )}
+
+      <div className="row pl-3 flex-grow-1 mt-2 justify-content-end">
+        <Share shareTitle="Invite" title={title} variant={'outline-dark'} />
+      </div>
     </div>
   </div>
 );
