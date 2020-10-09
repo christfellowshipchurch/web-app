005108c34bb80b40718a250b29c1c763365157d3
Ryan Davidson <ryan@differential.com>
2020-10-09 13:03:21 -0400

    TMP: Undo EventMedia null, that fixed it but not sure why...

--- src/content-single/EventContentItem/components/EventMedia.js
+++ src/content-single/EventContentItem/components/EventMedia.js
@@ -4,24 +4,25 @@ import { get } from 'lodash';
 
 import { Media } from 'ui';
 
-const EventMedia = ({ title, coverImage, videos, liveStreamSource }) => null;
-// <div className="mb-2">
-//   <Media
-//     imageUrl={get(coverImage, 'sources[0].uri', '')}
-//     videoUrl={
-//       !!liveStreamSource && liveStreamSource !== ''
-//         ? liveStreamSource
-//         : get(videos, '[0].sources[0].uri', '')
-//     }
-//     isLive={!!liveStreamSource && liveStreamSource !== ''}
-//     imageAlt={`${title} - ${get(coverImage, 'name', '')}`}
-//     className="shadow"
-//     ratio={{ xs: '16by9' }}
-//     forceRatio
-//     rounded
-//     showControls
-//   />
-// </div>
+const EventMedia = ({ title, coverImage, videos, liveStreamSource }) => (
+  <div className="mb-2">
+    <Media
+      imageUrl={get(coverImage, 'sources[0].uri', '')}
+      videoUrl={
+        !!liveStreamSource && liveStreamSource !== ''
+          ? liveStreamSource
+          : get(videos, '[0].sources[0].uri', '')
+      }
+      isLive={!!liveStreamSource && liveStreamSource !== ''}
+      imageAlt={`${title} - ${get(coverImage, 'name', '')}`}
+      className="shadow"
+      ratio={{ xs: '16by9' }}
+      forceRatio
+      rounded
+      showControls
+    />
+  </div>
+);
 
 EventMedia.propTypes = {
   title: PropTypes.string,
