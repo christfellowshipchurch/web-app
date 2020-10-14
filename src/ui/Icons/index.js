9de9d2e3f4b9e757220259fb089b39752dfbadc7
jsgraef <jg@joshgraef.com>
2020-10-14 16:10:42 -0400

    Add TV icon

--- src/ui/Icons/index.js
+++ src/ui/Icons/index.js
@@ -17,13 +17,14 @@ import Circle from './circle';
 import Clock from './clock';
 import Comments from './comments';
 import DotCircle from './dot-circle';
-import EnvelopeOpenDollar from './envelope-open-dollar';
 import Envelope from './envelope';
+import EnvelopeOpenDollar from './envelope-open-dollar';
 import ExclamationCircle from './exclamation-circle';
 import Facebook from './facebook';
 import Google from './google';
 import Handshake from './handshake';
 import Home from './home';
+import Icon from './Icon';
 import Instagram from './instagram';
 import LiveDot from './live-dot';
 import LockAlt from './lock-alt';
@@ -39,12 +40,12 @@ import SignOut from './sign-out';
 import Square from './square';
 import ThreeDots from './three-dots';
 import Times from './times';
+import Tv from './tv';
 import Twitter from './twitter';
-import UserCircle from './user-circle';
 import User from './user';
+import UserCircle from './user-circle';
 import Users from './users';
 import Youtube from './youtube';
-import Icon from './Icon';
 
 export {
   AngleDown,
@@ -64,13 +65,14 @@ export {
   Church,
   Clock,
   Comments,
-  EnvelopeOpenDollar,
   Envelope,
+  EnvelopeOpenDollar,
   ExclamationCircle,
   Facebook,
   Google,
   Handshake,
   Home,
+  Icon,
   Instagram,
   LockAlt,
   Microsoft,
@@ -83,11 +85,11 @@ export {
   Square,
   ThreeDots,
   Times,
+  Tv,
   Twitter,
   User,
   Users,
   Youtube,
-  Icon,
 };
 
 export const Icons = {
@@ -110,8 +112,8 @@ export const Icons = {
   Clock,
   Comments,
   DotCircle,
-  EnvelopeOpenDollar,
   Envelope,
+  EnvelopeOpenDollar,
   ExclamationCircle,
   Facebook,
   Google,
@@ -132,9 +134,10 @@ export const Icons = {
   Square,
   ThreeDots,
   Times,
+  Tv,
   Twitter,
-  UserCircle,
   User,
+  UserCircle,
   Users,
   Youtube,
 };
---
+++ src/ui/Icons/tv.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Icon = ({ size, fill }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      aria-labelledby="title"
+    >
+      <rect x="2" y="7" width="20" height="15" rx="2" ry="2"></rect>
+      <polyline points="17 2 12 7 7 2"></polyline>
+    </svg>
+  );
+};
+
+export default Icon;
+
+Icon.propTypes = {
+  size: PropTypes.number,
+  fill: PropTypes.string,
+};
+
+Icon.defaultProps = {
+  size: 32,
+};
