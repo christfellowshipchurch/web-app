2899724ad3f1390349e73070881f7879b0a82f9f
Ryan Davidson <ryan@differential.com>
2020-10-20 13:28:29 -0400

    Fix import path that got messed up in previous commit somehow

--- src/groups/index.sandbox.js
+++ src/groups/index.sandbox.js
@@ -1,3 +1,3 @@
 export { default as Groups } from './Groups';
-export { default as GroupContentItemConnected } from './GroupItemConnected';
+export { default as GroupContentItemConnected } from './NewGroupContentItemConnected';
 export { default as GroupListConnected } from './GroupListConnected';
