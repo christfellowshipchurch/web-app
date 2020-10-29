e83b8ef4526f8996badac297124f62eb833f2532
jsgraef <joshgraef@differential.com>
2020-10-29 14:02:46 -0400

    CTA as cards

--- src/content-single/EventContentItem/components/CallsToAction.js
+++ src/content-single/EventContentItem/components/CallsToAction.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { isEmpty } from 'lodash';
-
+import classnames from 'classnames';
 import { GoogleAnalytics } from 'analytics';
+import { Card, Row, Icon } from 'ui';
 
 const CallsToAction = ({ eventTitle, items, hasEvents }) => {
   if (isEmpty(items)) {
@@ -10,26 +11,44 @@ const CallsToAction = ({ eventTitle, items, hasEvents }) => {
   }
 
   return (
-    <div>
+    <>
       {hasEvents && <h3>Get Started</h3>}
-      {items.map(({ call, action }, index) => (
-        <a
-          key={`cta-${index}`}
-          className={'btn btn-primary btn-block my-3'}
-          href={action}
-          target={action.includes('http') ? '_blank' : ''}
-          onClick={() =>
-            GoogleAnalytics.trackEvent({
-              category: 'Event Item',
-              action: `${eventTitle} Call to Action`,
-              label: `${eventTitle} - ${call} Button`,
-            })
-          }
-        >
-          {call}
-        </a>
-      ))}
-    </div>
+      <Row>
+        {items.map(({ call, action }, index) => (
+          <a
+            key={`cta-${index}`}
+            href={action}
+            target={action.includes('http') ? '_blank' : ''}
+            onClick={() =>
+              GoogleAnalytics.trackEvent({
+                category: 'Event Item',
+                action: `${eventTitle} Call to Action`,
+                label: `${eventTitle} - ${call} Button`,
+              })
+            }
+            className={classnames('col-2', 'my-2', 'mx-2')}
+          >
+            <Card className={classnames('bg-white')}>
+              <div
+                className={classnames(
+                  'd-flex',
+                  'justify-content-center',
+                  'align-items-center',
+                  'flex-column'
+                )}
+              >
+                <div className="mb-1">
+                  <Icon name="church" size={32} />
+                </div>
+                <div>
+                  <span className={classnames('my-3')}>{call}</span>
+                </div>
+              </div>
+            </Card>
+          </a>
+        ))}
+      </Row>
+    </>
   );
 };
 
