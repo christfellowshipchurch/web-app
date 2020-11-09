79393c3c599682782640dfae204f5d8ad8adcef4
jsgraef <joshgraef@differential.com>
2020-11-09 16:32:46 -0500

    AccordionItem

--- src/ui/Accordion/AccordionItem.js
+++ src/ui/Accordion/AccordionItem.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
-import { Accordion as BootstrapAccordion, Card, Collapse } from 'react-bootstrap';
+import { Collapse } from 'react-bootstrap';
 import { AngleDown } from '../Icons';
 
 // Styling and layout for the physical accordion item
