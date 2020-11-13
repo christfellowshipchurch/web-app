2c5b6afeff0b40e1279940ad4a6e2be8e6be1164
jsgraef <joshgraef@differential.com>
2020-11-13 13:41:23 -0500

    Add useInteraction hook

--- src/mutations/index.js
+++ src/mutations/index.js
@@ -1,2 +1,10 @@
 import VIEW_ACTION from './interactViewAction';
-export { VIEW_ACTION };
+import useInteraction from './useInteraction';
+
+const ACTIONS = {
+  VIEW_ACTION: 'VIEW_ACTION',
+  GROUP_RESOURCE_READ_CONTENT: 'GROUP_RESOURCE_READ_CONTENT',
+  GROUP_RESOURCE_OPEN_URL: 'GROUP_RESOURCE_OPEN_URL',
+};
+
+export { VIEW_ACTION, ACTIONS, useInteraction };
