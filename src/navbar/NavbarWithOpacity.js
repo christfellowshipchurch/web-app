c6e42713b3ff650f7925a874a11431ce57e6aebe
dzwood <46049974+dzwood@users.noreply.github.com>
2020-12-02 16:50:56 -0500

    Update NavbarWithOpacity.js

--- src/navbar/NavbarWithOpacity.js
+++ src/navbar/NavbarWithOpacity.js
@@ -1,17 +1,18 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { useScrollPosition } from '../hooks';
 import Navbar from './index';
 
 const NavbarWithOpacity = ({ offset }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [bgColor, setBgColor] = useState('transparent');
 
-  useScrollPosition(({ currPos }) => {
-    const opaque = currPos.y > -1 * offset;
-
-    setBgColor(opaque ? 'transparent' : 'dark');
-  });
+  // **** Change Navbar color on ScrollPosition,
+  // **** not needed right now but can be used later if needed
+  //
+  // useScrollPosition(({ currPos }) => {
+  //   const opaque = currPos.y > -1 * offset;
+  //   setBgColor(opaque ? 'transparent' : 'dark');
+  // });
 
   return (
     <Navbar
