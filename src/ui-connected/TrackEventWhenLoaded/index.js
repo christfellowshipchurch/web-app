4da541fd558ba635c4b8c0c0107d919f8bfc6908
Caleb Panza <caleb.panza@icloud.org>
2021-01-15 15:25:58 -0500

    TrackEventWhenLoaded

---
+++ src/ui-connected/TrackEventWhenLoaded/index.js
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { useMutation } from 'apollo-client';
+import gql from 'graphql-tag';
+
+const TRACK = gql`
+  mutation track($properties: [AnalyticsMetaField]!, $eventName: String!) {
+    track(properties: $properties, eventName: $eventName) @client
+  }
+`;
+
+const objectToGqlInput = (props = {}) =>
+  Object.keys(props).map((key) => ({
+    field: key,
+    value: props[key],
+  }));
+
+const TrackEventWhenLoadedConnected = ({ loaded, eventName, properties }) => {
+  const [track] = useMutation(TRACK);
+
+  useEffect(() => {
+    if (loaded) {
+      track({
+        variables: {
+          eventName,
+          properties: objectToGqlInput(properties),
+        },
+      });
+    }
+  }, [loaded]);
+
+  return null;
+};
+
+TrackEventWhenLoadedConnected.propTypes = {
+  loaded: PropTypes.bool,
+  eventName: PropTypes.string.isRequired,
+  // eslint-disable-next-line react/forbid-prop-types
+  properties: PropTypes.any,
+};
+
+export default TrackEventWhenLoadedConnected;
