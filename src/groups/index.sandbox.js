60d9236a1df5a643ca3a26e78878732b87d2b194
asleepingpanda <45076058+asleepingpanda@users.noreply.github.com>
2021-02-26 16:30:47 -0500

    [CFDP-1180] Launch groups layout (#245)
    * Fixed Group Member name overflow
    
    * Enable sandbox file for prod
    
    Co-authored-by: Caleb Panza <caleb.panza@icloud.org>

--- src/groups/index.sandbox.js
+++
@@ -1,3 +0,0 @@
-export { default as Groups } from './Groups';
-export { default as GroupContentItemConnected } from './NewGroupContentItemConnected';
-export { default as GroupListConnected } from './GroupListConnected';
